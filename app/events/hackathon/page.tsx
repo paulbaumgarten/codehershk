bb8190df99a4ef231380afefb7fe93df5be867af
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2025-03-01 15:01:44 +0800

    Update page.tsx

--- app/events/hackathon/page.tsx
+++ app/events/hackathon/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, ReactNode } from "react";
 import { useId } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
