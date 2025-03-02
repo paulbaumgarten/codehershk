b92fa56e7b95b7b573a90ba8dbcd48edaf6f66bf
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2025-03-02 23:13:24 +0800

    fix link

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -54,7 +54,7 @@ export default function CurrentEventsPage() {
             <div className="relative inline-flex items-center justify-center ease-out group">
               <div className="absolute duration-1000 transition-all opacity-70 -inset-px bg-primary-500 blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-300 animate-tilt"></div>
               <Link
-                href="https://forms.gle/aPcWkbQxVxUh2GPj8"
+                href="https://forms.gle/oyNfxVrsFquozApS9"
                 className="relative top-0 inline-flex items-center justify-center w-full px-6 py-2 text-base font-medium leading-loose text-center text-white duration-300 rounded-lg bg-primary-500 hover:-top-2 transition-all"
                 target="_blank"
               >
