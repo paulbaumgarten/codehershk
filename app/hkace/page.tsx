397e0eac690f2961c332dcdb7013a83182771a1c
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2026-02-07 11:07:18 +0800

    trying to fix the hkace slug

--- app/hkace/page.tsx
+++ app/hkace/page.tsx
@@ -11,7 +11,7 @@ export default function RedirectToMain() {
     const trackAndRedirect = async () => {
       try {
         await fetch('https://app.codehershk.org/hkace', {
-          method: 'GET',
+          method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
